7516ed5cffd3e749bf4832ac2c4a4f7316cb15c6
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-11-13 10:22:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://ljffexrs:nQNYkbGGeI-2-kHqoXJoL_ybuogzaxRl@trumpet.db.elephantsql.com/ljffexrs")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001976588675"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002018123667"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001594692005"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
